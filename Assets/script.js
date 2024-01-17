bab719368bdb7abac3e538a22207efcb911ab5b4
jdporr <jdporrazzo@gmail.com>
2024-01-17 16:35:03 -0500

    created function and defined variables within getPassword

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//create a function to get the randomized password and define variables as strings for possible password character options
+  function getPassword() {
+    var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    var numbers = "0123456789";
+    var specialChar = "!@#$%^&*()-=+<>?/|':[]{}";
+    var randSelect = "";
+    var randKey = "";
+
+  }
+
+
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
